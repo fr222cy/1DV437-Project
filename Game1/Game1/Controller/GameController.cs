145d38fac4c20bb4d5535c9e614004f983d1b080
fr222cy <fr222cy@student.lnu.se>
2015-12-10 15:04:36 +0100

    Basic Controller Structure implemented.

---
+++ Game1/Game1/Controller/GameController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Game1.Controller
+{
+    class GameController
+    {
+
+        public GameController()
+        {
+
+        }
+
+        public void LoadContent(SpriteBatch sBatch)
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw()
+        {
+
+        }
+    }
+}
---
+++ Game1/Game1/Controller/Mastercontoller.cs
@@ -0,0 +1,97 @@
+﻿using Game1.Controller;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Game1
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Mastercontoller : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+
+        GameController gameController;
+        MenuController menuController;
+
+        enum Gamestate
+        {
+            Menu,
+            Playing,
+        }
+
+        Gamestate CurrentGameState = Gamestate.Menu;
+
+        public Mastercontoller()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        
+        protected override void Initialize()
+        {
+          
+
+            base.Initialize();
+        }
+
+      
+        protected override void LoadContent()
+        {
+           
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            //Loads Menu Content.
+            menuController.LoadContent(spriteBatch);
+
+            //Loads Game Content.
+            gameController.LoadContent(spriteBatch);
+
+  
+        }
+
+     
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+     
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            switch(CurrentGameState)
+            {
+                case Gamestate.Menu:
+                    break;
+
+                case Gamestate.Playing:
+                    break;
+            }
+
+            base.Update(gameTime);
+        }
+
+      
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            switch (CurrentGameState)
+            {
+                case Gamestate.Menu:
+                    break;
+
+                case Gamestate.Playing:
+                    break;
+            }
+
+            base.Draw(gameTime);
+        }
+    }
+}
