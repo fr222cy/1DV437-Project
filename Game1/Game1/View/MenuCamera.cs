2295023e7a4464d01a3c44eec5d7d24a964ba6a7
fr222cy <fr222cy@student.lnu.se>
2015-12-10 17:14:54 +0100

    MenuCamera.cs handles the Menu scaling

---
+++ Game1/Game1/View/MenuCamera.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Game1.View
+{
+    class MenuCamera
+    {
+
+        private float scaleX;
+        private float scaleY;
+        private float scale;
+        
+
+        public MenuCamera(Viewport port)
+        {
+
+            scaleX = port.Width;
+            scaleY = port.Height;
+
+            if(scaleX > scaleY)
+            {
+                scale = scaleX;
+            }
+            else
+            {
+                scale = scaleY;
+            }
+
+        }
+
+        public Vector2 getViewCoords(Vector2 position, float textureWidth, float textureHeight)
+        {
+            float screenX = (scaleX * position.X);
+            float screenY = (scaleY * position.Y);
+
+            return new Vector2(screenX, screenY);
+        }
+
+        public Vector2 getClickModelCoords(Vector2 position)
+        {
+
+            float logicalX = (position.X / scaleX);
+            float logicalY = (position.Y / scaleY);
+
+            return new Vector2(logicalX, logicalY);
+        }
+
+
+        public float getScale(float width)
+        {
+            return scale / width ;
+        }
+
+
+
+    }
+}
+
